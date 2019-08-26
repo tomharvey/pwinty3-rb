d1c76ebfb9fed20b35621a146d64bba94d8e01e6
Thomas Harvey <tom@alush.co.uk>
2019-08-26 03:54:59 +0200

    reducing replication of env access

--- lib/pwinty3/http_errors.rb
+++ lib/pwinty3/http_errors.rb
@@ -1,11 +1,12 @@
 module Pwinty3
 	class HttpErrors < Faraday::Response::Middleware
 		def on_complete(env)
+			msg = env[:body]
 			case env[:status]
-			when 401; raise Pwinty3::AuthenticationError, env[:body]
-			when 403; raise Pwinty3::StateIsInvalid, env[:body]
-			when 404; raise Pwinty3::OrderNotFound, env[:body]
-			when 500; raise Pwinty3::Error, env[:body]
+			when 401; raise Pwinty3::AuthenticationError, msg
+			when 403; raise Pwinty3::StateIsInvalid, msg
+			when 404; raise Pwinty3::OrderNotFound, msg
+			when 500; raise Pwinty3::Error, msg
 			end
 		end
 	end
