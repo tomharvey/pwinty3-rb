2fc6cb03898054d94b53adfb8ddcd960d0766f31
Thomas Harvey <tom@alush.co.uk>
2019-08-26 04:00:44 +0200

    spaces instead of tabs

--- lib/pwinty3.rb
+++ lib/pwinty3.rb
@@ -13,28 +13,28 @@ require "pwinty3/shipping_info"
 require "pwinty3/version"
 
 module Pwinty3
-	class Error < StandardError; end
-	class AuthenticationError < Pwinty3::Error; end
-	class OrderNotFound < Pwinty3::Error; end
-	class StateIsInvalid < Pwinty3::Error; end
+    class Error < StandardError; end
+    class AuthenticationError < Pwinty3::Error; end
+    class OrderNotFound < Pwinty3::Error; end
+    class StateIsInvalid < Pwinty3::Error; end
 
-	MERCHANT_ID = ENV['PWINTY3_MERCHANT_ID']
-	API_KEY = ENV['PWINTY3_API_KEY']
-	BASE_URL = ENV['PWINTY3_BASE_URL'] || 'https://sandbox.pwinty.com'
-	API_VERSION = 'v3.0'
+    MERCHANT_ID = ENV['PWINTY3_MERCHANT_ID']
+    API_KEY = ENV['PWINTY3_API_KEY']
+    BASE_URL = ENV['PWINTY3_BASE_URL'] || 'https://sandbox.pwinty.com'
+    API_VERSION = 'v3.0'
 
-	HEADERS = {
-		'X-Pwinty-MerchantId' => Pwinty3::MERCHANT_ID,
-		'X-Pwinty-REST-API-Key' => Pwinty3::API_KEY,
-	}
+    HEADERS = {
+        'X-Pwinty-MerchantId' => Pwinty3::MERCHANT_ID,
+        'X-Pwinty-REST-API-Key' => Pwinty3::API_KEY,
+    }
 
-	def self.conn
-		url = "#{Pwinty3::BASE_URL}/#{Pwinty3::API_VERSION}/"
-		Faraday.new(url: url, headers: Pwinty3::HEADERS) do |config|
-  			config.request :json
-  			config.response :json
-  			config.use Pwinty3::HttpErrors
-  			config.adapter Faraday.default_adapter
-		end
-	end
+    def self.conn
+        url = "#{Pwinty3::BASE_URL}/#{Pwinty3::API_VERSION}/"
+        Faraday.new(url: url, headers: Pwinty3::HEADERS) do |config|
+            config.request :json
+            config.response :json
+            config.use Pwinty3::HttpErrors
+            config.adapter Faraday.default_adapter
+        end
+    end
 end
