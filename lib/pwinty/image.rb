b96a5f41e8c7b7025d1ad51a750f18610392c5fd
Richard Peck <rpeck@frontlineutilities.co.uk>
2020-01-17 12:24:30 +0000

    Update image.rb

--- lib/pwinty/image.rb
+++ lib/pwinty/image.rb
@@ -12,13 +12,13 @@ module Pwinty
 		attribute :previewUrl, Types::String.optional
 		attribute :thumbnailUrl, Types::String.optional
 		attribute :sku, Types::String
-		attribute :attributes, Types::Hash.schema(
-			substrateWeight: Types::String.optional,
-			frame: Types::String.optional,
-			edge: Types::String.optional,
-			paperType: Types::String.optional,
-			frameColour: Types::String.optional,
-		).optional
+		#attribute :attributes, Types::Hash.schema(
+		#	substrateWeight: Types::String.optional,
+		#	frame: Types::String.optional,
+		#	edge: Types::String.optional,
+		#	paperType: Types::String.optional,
+		#	frameColour: Types::String.optional,
+		#).optional
 		attribute :errorMessage, Types::String.optional
 	end
 end
